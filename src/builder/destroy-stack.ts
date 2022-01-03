922be942eec57643e831794783e98b06b89f00e2
Ray Epps <rayharryepps@gmail.com>
2022-01-03 14:27:40 -0700

    add scripts to destroy a stack

--- src/builder/destroy-stack.ts
+++ src/builder/destroy-stack.ts
@@ -25,21 +25,15 @@ const main = _.defered(async ({ defer, deploymentId }: Args & { defer: Defer }) 
   //
   const logFilePath = `${config.logDir}/${_.dashCase(deploymentId)}.log`
   await fs.writeFile(logFilePath, '')
-  const logStream = fs.createWriteStream(logFilePath)
-  process.stdout.write = process.stderr.write = logStream.write.bind(logStream)
+  // const logStream = fs.createWriteStream(logFilePath)
+  // process.stdout.write = process.stderr.write = logStream.write.bind(logStream)
 
 
   defer((err) => {
     const logs = fs.readFileSync(logFilePath, 'utf-8')
     api.deployments.updateStatus({
       deploymentId,
-      status: (() => {
-        if (!err) return 'success'
-        if (/\+\s+?aws.+?created/.test(logs) || /~\s+?aws.+?updated/.test(logs)) {
-          return 'partial_success'
-        }
-        return 'failed'
-      })(),
+      status: err ? 'failed' : 'success',
       source: 'exo.builder.deploy',
     }, { token: config.exobaseToken })
     api.deployments.updateLogs({
@@ -114,93 +108,20 @@ const main = _.defered(async ({ defer, deploymentId }: Args & { defer: Defer }) 
 
 
   //
-  //  Download Source
-  //
-  //  GitHub gives us a zip that contains a single folder with a name
-  //  like '{owner}-{repo}-{commit_hash}'. We don't have any easy way
-  //  to figure out what the hash is so we use `getSourceDirName` to
-  //  get the name of that directory. We then rename it to 'source' as
-  //  our pulumi template scripts will expect.
-  //
-  const linkResponse = await api.services.getSourceDownloadLink({
-    serviceId,
-    platformId,
-    deploymentId
-  }, { token: config.exobaseToken })
-  if (linkResponse.error) {
-    console.error(linkResponse.error)
-    return
-  }
-  await downloadZipFile({
-    url: linkResponse.data.url,
-    path: `${workingDir}/source.zip`
-  })
-  await cmd(`unzip source.zip`, {
-    cwd: `${workingDir}`,
-    quiet: true
-  })
-  const sourceDirName = await getSourceDirName(`${workingDir}/source.zip`)
-  await fs.rename(`${workingDir}/${sourceDirName}`, `${workingDir}/source`)
-  const functions = service.type === 'api' && exobuilds.getFunctionMap({
-    path: `${workingDir}/source`,
-    ext: 'ts'
-  })
-  const pj = await fs.readJson(`${workingDir}/source/package.json`)
-  const version = pj?.version ?? ''
-
-
-  // 
-  //  Start Pulumi deploy & get the outputs
-  //
-  //  If the stack has already been created the init will fail, but the
-  //  select will succeed. If the stack has not been created the init
-  //  will succeed but the select will fail. Ignoring some errors here
-  //  so we don't have to do more work.
+  //  Run Pulumi Destroy to tear down all infrastructure
   //
   const stackName = safeName(context.service.id)
-  await cmd(`pulumi stack init ${stackName}`, {
+  await cmd(`pulumi destroy --yes --stack ${stackName}`, {
     cwd: workingDir
   })
-  await cmd(`pulumi stack select ${stackName}`, {
-    cwd: workingDir
-  })
-  const [upErr] = await cmd('pulumi up --yes', {
-    cwd: workingDir
-  })
-  if (upErr !== null) {
-    console.error('The Pulumi deployment stack failed to deploy. Check for errors just above this.')
-    throw 'Pulumi up failed'
-  }
-  const [outputErr, stackOutput] = await cmd('pulumi stack output --json', {
-    cwd: workingDir,
-    buffer: true
-  })
-  if (outputErr !== null) {
-    console.error('The Pulumi stack failed to provide outputs. Check for errors just above this.')
-    throw 'Pulumi outputs failed'
-  }
-
-  const output = stackOutput && stackOutput.length > 2
-    ? JSON.parse(stackOutput) as any
-    : { default: { out: {} } }
 
 
-
-  // 
-  //  Update service attributes with the Pulumi outputs  
   //
-  await api.deployments.updateAttributes({
-    deploymentId,
-    attributes: {
-      version,
-      url: output.default.out.url,
-      outputs: output.default.out,
-      functions: service.type === 'api' ? functions.map(f => ({
-        module: f.module,
-        function: f.function
-      })) : []
-    }
-  }, { token: config.exobaseToken })
+  //  Run Pulumi rm to clear all state from pulumi
+  //
+  await cmd(`pulumi stack rm --yes ${stackName}`, {
+    cwd: workingDir
+  })
 
 
   // 
@@ -232,30 +153,6 @@ const replaceInFile = async ({
   const content = await fs.readFile(file, 'utf-8')
   const newContent = content.replace(find, replacement)
   await fs.writeFile(file, newContent)
-}
-
-const downloadZipFile = async ({
-  url,
-  path
-}: {
-  url: string
-  path: string
-}): Promise<any> => {
-  const finished = promisify(stream.finished)
-  const writer = fs.createWriteStream(path)
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  })
-  response.data.pipe(writer)
-  return await finished(writer)
-}
-
-const getSourceDirName = async (zipPath: string) => {
-  const zipFileData = await fs.readFile(zipPath)
-  const zip = await JSZip.loadAsync(zipFileData)
-  return Object.keys(zip.files)[0]
 }
 
 const listDirsInDir = async (dirPath: string) => {
